9978041ac42f846c598e10a39f8590c75224de9b
Elise Erickson <elisece717@gmail.com>
2023-11-21 15:27:47 -0500

    satisfies tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => array.slice(0,2);
+const returnLastTwoDrivers = (array) => array.slice(-2);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const selectDifferentDrivers = (array, selectionFunc) => {
+    return selectionFunc(array);
+}
+
+function createFareMultiplier(factor){
+    return (fare) => (fare * factor);
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
